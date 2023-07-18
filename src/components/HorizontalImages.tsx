b9fb3addcdac8ee7e69c994445941352a1ff812f
Thomas Booth <thomasbooth26@gmail.com>
2023-07-18 22:33:02 +0100

    update

--- src/components/HorizontalImages.tsx
+++ src/components/HorizontalImages.tsx
@@ -12,32 +12,30 @@ const HorizontalImages = () => {
       target: containerRef,
     })
 
-    const x = useTransform(scrollYProgress, [0.2, 0.8], ['0vw', '-330vw'])
+    const x = useTransform(scrollYProgress, [0, 0.8], ['0vw', '-320vw'])
 
-    const scale = useTransform(scrollYProgress, [0, 0.2, 0.8, 1], [0.6, 1, 1, 0.6])
+    const scale = useTransform(scrollYProgress, [0, 0.8, 1], [1, 1, 0.6])
     const opacity = useTransform(scrollYProgress, [0, 0.000001, 0.2], [0, 1, 0])
     
   return (
+    <>
     <section ref = {containerRef} >
-        <div className = 'relative h-[600vh]'>
+        <div className = 'relative h-[600vh] '>
             <div className = 'sticky top-0 w-[600vw] text-[10vw] font-bold text-gray-100'>
                 <motion.div
                 style = {{x}}
                 className = 'flex'>
                     <motion.div style = {{scale}}
-                        className = 'flex justify-center items-center h-screen bg-otter-back bg-cover bg-center w-[100vw]'>
+                        className = 'rounded-t-3xl bg-white h-[100vh] w-[100vw]'>
                         
                     </motion.div>
-                    <div className = 'w-[10vw]'>
-
-                    </div>
-                    <div className = 'flex justify-center items-center h-screen bg-otter-back bg-cover bg-center w-[100vw]'>
+                    <div className = 'flex justify-center items-center h-screen  w-[100vw]'>
                         IMAGE 2
                     </div>
                     <div className = 'w-[10vw]'>
 
                     </div>
-                    <div className = 'flex justify-center items-center h-screen bg-otter-back bg-cover bg-center w-[100vw]'>
+                    <div className = 'flex justify-center items-center h-screen  w-[100vw]'>
                         IMAGE 3
                     </div>
                     <div className = 'w-[10vw]'>
@@ -52,6 +50,7 @@ const HorizontalImages = () => {
             </div>
         </div>
     </section>
+    </>
   )
 }
 
--- src/components/Navlinks.tsx
+++ src/components/Navlinks.tsx
@@ -12,11 +12,11 @@ const Navlinks = () => {
     target: targetRef,
   })
   const y = useTransform(scrollYProgress, [0, 0.8], ['0vh', '100vh'])
-  const scale = useTransform(scrollYProgress, [0.2, 0.8], [1, 0])
+  const scale = useTransform(scrollYProgress, [0.2, 0.8], [0.7, 0.6])
   
   return (
     <div >
-      <img src = '/images/18-Grizzlies-at-dawn.jpg' alt = 'grizzly' className = 'sticky top-0 w-[100vw]'/>
+      <img src = '/images/18-Grizzlies-at-dawn.jpg' alt = 'grizzly' className = 'sticky saturate-10 top-0 w-[200vw]'/>
       <div>
         <div ref = {targetRef} className = 'pointer-events-none absolute text-[10vw] font-bold z-20 top-0 h-[200vh]'>
           <motion.div 
@@ -31,7 +31,7 @@ const Navlinks = () => {
         </div>
       </div>
       <motion.div 
-        className = 'transform ml-[10vw] rotate-x-[-5deg] rotate-z-[-5deg] h-screen flex flex-col items-start  z-20'>
+        className = 'transform ml-[10vw] rotate-x-[-5deg] rotate-z-[-5deg] h-[100vh] flex flex-col items-start  z-20'>
           <Text3D primary = {"Polar"} secondary = {"Arctic"}/>
           <Text3D primary = {"Magestic"} secondary = {"Tigers"}/>
           <Text3D primary = {"Wild"} secondary = {"Alaska"}/> 
