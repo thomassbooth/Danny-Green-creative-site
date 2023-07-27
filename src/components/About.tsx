8ebbd854cc508a60a6501c4a861800507e91c0cc
Thomas Booth <thomasbooth26@gmail.com>
2023-07-27 23:09:08 +0100

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
     <div className = 'flex flex-col justify-center leading-none tracking-tight items-center text-[#171717] h-screen w-screen'>
         <span className = {`${libre.className} italic font-extralight text-[2.5vw]`}>Danny Who?</span>
         <span className = ' z-20 text-center font-[800] tracking-[-.075em] text-[10vw] uppercase'>HE TAKES</span>
-        <span className = 'mb-10 z-20 text-center font-[800] leading-[4.5rem] tracking-[-.075em] text-[10vw] uppercase'>PHOTOS</span>
+        <span className = 'mb-10 z-20 text-center font-[800] leading-[10vh] tracking-[-.075em] text-[10vw] uppercase'>PHOTOS</span>
         <span className = {`${libre.className} font-light text-[3vw]`}>30 years + of experience</span>
         <span className = {`${libre.className} font-light text-[3vw]`}>Widely published around the world</span>
         <span className = {`${libre.className} font-light text-[3vw]`}>Base in Leicestershire, UK</span>
