b47ad6548e74ed55c52c375cff95450c388fd48f
Thomas Booth <thomasbooth26@gmail.com>
2023-08-02 22:27:56 +0100

    update font

--- src/components/GalleryLink.tsx
+++ src/components/GalleryLink.tsx
@@ -13,7 +13,7 @@ const GalleryLink: React.FC<galleryLinkProps> = ({imgSrc, link, name}) => {
     <Link href = {link} className = 'duration-300 group relative h-[45vh] grayscale hover:grayscale-0 transition-all '>
         <div className = 'flex z-50 overflow-hidden opacity-80 text-pastel-gray-light transition-all duration-500  justify-center items-end w-full h-full absolute'>
           <div className = 'px-3 pt-1 translate-y-20 group-hover:translate-y-0 transition-all duration-500 text-center bg-background-gray'>
-            <h1 className = 'uppercase font-bold tracking-wider'>{name}</h1>
+            <h1 className = 'uppercase text-[1vw] font-bold tracking-wider'>{name}</h1>
           </div>
         </div>
         <Image src = {`/images/${imgSrc}`} alt = 'froggy' fill/>
