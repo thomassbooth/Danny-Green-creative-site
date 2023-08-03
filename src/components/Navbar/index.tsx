3fe91151e53ac6aaba6b5ea14d2a398e3ca37357
Thomas Booth <thomasbooth26@gmail.com>
2023-08-04 00:20:19 +0100

    update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -28,8 +28,8 @@ useEffect(() => {
 return (
     <nav className={`mix-blend-difference text-pastel-gray-light w-full overflow-hidden transition-all duration-500 fixed z-50 ${visible ? 'translate-y-0' : '-translate-y-20'} `}>
         <ul className = 'mx-5 mt-5 flex justify-between gap-4 items-center'>
-          <li className = 'text-[1.2vw] uppercase font-bold'>
-            <h2>DannyG</h2>
+          <li className = 'text-[1.2vw] uppercase font-light'>
+            <h2>DG</h2>
           </li>
           <li className = 'flex gap-10 uppercase text-[1.2vw] font-light'>
             <Link title = 'Home'/>
