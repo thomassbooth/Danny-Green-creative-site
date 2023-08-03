bffe7558ee0682038f1fd720d1c57fd9b253fa10
Thomas Booth <thomasbooth26@gmail.com>
2023-08-04 00:29:15 +0100

    update

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -33,7 +33,7 @@ const Link: React.FC<linkProps> = ({ title }) => {
         className="relative cursor-pointer inline-block overflow-hidden">
         <h2>{title}</h2>
         <motion.div
-          className="absolute left-0 w-full h-[1px] bg-pastel-gray-light bottom-0"
+          className="absolute left-0 w-full h-[1.5px] bg-pastel-gray-light bottom-0"
           animate={hoverState}
           variants={underlineVariants}
           transition={{ duration: 0.4 }}
