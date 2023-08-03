14b3203db65ca35d4188e541013e5658f7d80210
Thomas Booth <thomasbooth26@gmail.com>
2023-08-04 00:19:16 +0100

    navbar introduce

---
+++ src/components/Navbar/Link.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import { useState } from 'react';
+
+const underlineVariants = {
+    idle: {
+        x: '-3.6vw'
+    },
+    enter: {
+        x: '3.6vw'
+    },
+    exit: {
+        x: '-3.6vw'
+    }
+}
+
+interface linkProps {
+    title: string
+}
+
+const Link: React.FC<linkProps> = ({ title }) => {
+
+    const [hoverState, setHoverState] = useState('idle')
+
+    console.log(hoverState)
+    return (
+      <motion.div 
+        onHoverStart={(e) => {setHoverState('enter')}}
+        onHoverEnd={(e) => {
+            setHoverState('exit')
+        }}
+        className="relative cursor-pointer inline-block overflow-hidden">
+        <h2>{title}</h2>
+        <motion.div
+          className="absolute left-0 w-full h-[1px] bg-pastel-gray-light bottom-0"
+          animate={hoverState}
+          variants={underlineVariants}
+          transition={{ duration: 0.4 }}
+        />
+      </motion.div>
+    );
+  };
+
+export default Link
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, {useState, useEffect} from 'react'
+import Link from './Link';
 
 const Navbar = () => {
 
@@ -25,9 +26,19 @@ useEffect(() => {
   }, []);
 
 return (
-    <div className={`text-pastel-gray-light w-full h-14 overflow-hidden transition-all duration-500 fixed z-50 ${visible ? 'translate-y-0' : '-translate-y-20'} `}>
-        Danny Green
-    </div>
+    <nav className={`mix-blend-difference text-pastel-gray-light w-full overflow-hidden transition-all duration-500 fixed z-50 ${visible ? 'translate-y-0' : '-translate-y-20'} `}>
+        <ul className = 'mx-5 mt-5 flex justify-between gap-4 items-center'>
+          <li className = 'text-[1.2vw] uppercase font-bold'>
+            <h2>DannyG</h2>
+          </li>
+          <li className = 'flex gap-10 uppercase text-[1.2vw] font-light'>
+            <Link title = 'Home'/>
+            <Link title = 'About'/>
+            <Link title = 'Gallery'/>
+            <Link title = 'Contact'/>
+          </li>
+        </ul>
+    </nav>
 )
 }
 
