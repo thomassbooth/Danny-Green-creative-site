e55b9f8ee29d6cb1346439e803e14e3c829b6583
Thomas Booth <thomasbooth26@gmail.com>
2023-08-03 21:48:52 +0100

    update to sizing

--- src/components/GalleryLink.tsx
+++ src/components/GalleryLink.tsx
@@ -12,7 +12,7 @@ const GalleryLink: React.FC<galleryLinkProps> = ({imgSrc, link, name}) => {
   return (
     <Link href = {link} className = 'duration-300 group relative h-[45vh] grayscale hover:grayscale-0 transition-all '>
         <div className = 'flex z-50 overflow-hidden opacity-80 text-pastel-gray-light transition-all duration-500  justify-center items-end w-full h-full absolute'>
-          <div className = 'px-3 pt-1 h-[4vh] translate-y-[5vh] group-hover:translate-y-0 transition-all duration-500 text-center bg-background-gray'>
+          <div className = 'px-3 pt-1 translate-y-[5vh] group-hover:translate-y-0 transition-all duration-500 text-center bg-background-gray'>
             <h1 className = 'uppercase text-[1vw] font-bold tracking-wider'>{name}</h1>
           </div>
         </div>
