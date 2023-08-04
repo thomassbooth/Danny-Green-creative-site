1f386092598443968b2ec9fa17a138316604be6a
Thomas Booth <thomasbooth26@gmail.com>
2023-08-04 17:32:23 +0100

    update

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -5,13 +5,13 @@ import { useState } from 'react';
 
 const underlineVariants = {
     idle: {
-        x: '-3.2vw'
+        x: '-3vw'
     },
     enter: {
-        x: '3.2vw'
+        x: '3vw'
     },
     exit: {
-        x: '-3.2vw'
+        x: '-3vw'
     }
 }
 
@@ -33,7 +33,7 @@ const Link: React.FC<linkProps> = ({ title }) => {
         className="relative cursor-pointer inline-block overflow-hidden">
         <h2>{title}</h2>
         <motion.div
-          className="absolute left-0 w-full h-[1.5px] bg-pastel-gray-light bottom-0"
+          className="absolute left-0 w-full h-[0.2vh] bg-pastel-gray-light bottom-0"
           animate={hoverState}
           variants={underlineVariants}
           transition={{ duration: 0.4 }}
