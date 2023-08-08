a3f090f818ff77a692a0116ac0aaaeec9e6bdf7d
Thomas Booth <thomasbooth26@gmail.com>
2023-08-08 22:26:15 +0100

    update

--- src/components/Awards/Project.tsx
+++ src/components/Awards/Project.tsx
@@ -10,9 +10,9 @@ interface projectProps {
 
 const Project: React.FC<projectProps> = ({setModal, index, title, place}) => {
   return (
-    <div className = 'group hover:opacity-50 flex w-full justify-between items-center pt-[50px] pb-[100px] pl-[50px] pr-[100px] border-t border-[#C9C9C9] transition-all duration-200 cursor-pointer last-of-type:border-b'
+    <div className = 'group hover:opacity-50 flex w-full justify-between items-center py-[50px] px-[50px] border-t border-[#C9C9C9] transition-all duration-200 cursor-pointer last-of-type:border-b'
         onMouseEnter={() => {setModal({active: true, index})}} onMouseLeave={() => {setModal({active: false, index})}}>
-        <h2 className = 'group-hover:translate-x-[-10px] m-0 transition-all duration-[0.4] font-normal text-6xl'>{title}</h2>
+        <h2 className = 'group-hover:translate-x-[-10px] m-0 mr-5 transition-all duration-[0.4] font-normal text-6xl'>{title}</h2>
         <p className = 'group-hover:translate-x-[10px] transition-all duration-[0.4]'>{place}</p>
     </div>
   )
