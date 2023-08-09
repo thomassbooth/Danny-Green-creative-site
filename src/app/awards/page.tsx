5e784f776641fc9b38de2e84a0d6af267e2e65dd
Thomas Booth <thomasbooth26@gmail.com>
2023-08-09 22:38:48 +0100

    more awards

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -11,55 +11,91 @@ const awards = [
     place: 'Highly Commended',
     src: 'Mountain-Hare-BWPA.jpeg',
     year: '2023',
-    color: '#FDF4F5'
+    color: '#EDF1F5'
   },
   {
     title: 'Bird Photographer of the Year',
     place: 'Commended',
     year: '2023',
-    src: 'Angel-of-the-North-BPOTY.jpeg'
+    src: 'Angel-of-the-North-BPOTY.jpeg',
+    color: '#ECF2F4'
   },
   {
     title: 'European Wildlife Photographer of the Year',
     place: 'Highly Commended',
     src: 'Arctic_Fox_with_a_Ringed_Seal_Pup_GDT.jpeg',
     year: '2023',
-    color: '#AEE2FF'
+    color: '#A0CDF1'
   },
   {
     title: 'European Wildlife Photographer of the Year',
     place: 'Category Winner',
     src: 'Red_Deer_Stag_shaking_water_of_itself.jpeg',
     year: '2021',
-    color: '#AAC8A7'
+    color: '#424402'
   },
   {
     title: 'Natures Best',
     place: 'Highly Honoured',
     src: 'Grizzly-at-Dawn.jpeg',
     year: '2016',
-    color: '#AAC8A7'
+    color: '#ED9033'
   },
   {
     title: 'British Wildlife Photography Awards',
     place: 'Highly Commended',
     src: 'Pine-Marten-portrait.jpeg',
     year: '2016',
-    color: '#AAC8A7'
+    color: '#839439'
   },
   {
     title: 'British Wildlife Photography Awards',
     place: 'Highly Commended',
     src: 'Pine-martens-play-fighting.jpeg',
     year: '2015',
-    color: '#AAC8A7'
+    color: '#B9935E'
+  },
+  {
+    title: 'British Wildlife Photography Awards',
+    place: 'Highly Commended',
+    src: 'Pine-Marten-jumping-towards-me.jpeg',
+    year: '2015',
+    color: '#C8D080'
+  },
+  {
+    title: 'British Wildlife Photography Awards',
+    place: 'Highly Commended',
+    src: 'Pine-Marten.jpeg',
+    year: '2015',
+    color: '#596755'
+  },
+  {
+    title: 'British Wildlife Photography Awards',
+    place: 'Highly Commended',
+    src: 'Mountain-Hare.jpeg',
+    year: '2015',
+    color: '#BEBFC4'
   },
   {
     title: 'British Wildlife Photography Awards',
     place: 'Highly Commended',
     src: 'Common-Seal-pup-in-a-sandstorm.jpeg',
     year: '2015',
-    color: '#AAC8A7'
+    color: '#B3937D'
+  },
+  {
+    title: 'British Wildlife Photography Awards',
+    place: 'Highly Commended',
+    src: 'Puffins-courtship-displaying.jpeg',
+    year: '2015',
+    color: '#06050B'
+  },
+  {
+    title: 'GDT - European Nature Photographer',
+    place: 'Highly Commended',
+    src: 'Litle-Auks-in-flight-and-blue-Iceberg.jpeg',
+    year: '2011',
+    color: '#96C2D1'
   },
 
 ]
