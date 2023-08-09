77ad715b510506595d1098b4971c5768eac8bd62
Thomas Booth <thomasbooth26@gmail.com>
2023-08-09 23:04:53 +0100

    awards animation in

--- src/components/Awards/Project.tsx
+++ src/components/Awards/Project.tsx
@@ -11,11 +11,27 @@ interface projectProps {
     year: string
 }
 
+const slideIn = {
+  initial: (i: number) => ({
+    x: ((-1)**i) * 120 + '%'
+  }),
+  open: (i: number) => ({
+    x: '0%',
+    transition: {duration: 0.5, delay: 0.15 * i}
+  })
+}
+
 const Project: React.FC<projectProps> = ({setModal, index, title, place, year}) => {
   return (
-    <motion.li className = 'group hover:opacity-50 flex w-full justify-between items-center py-[4vh] px-[50px] border-t border-[#C9C9C9] transition-all duration-200 cursor-pointer last-of-type:border-b'
+    <motion.li 
+        custom = {index}
+        variants = {slideIn}
+        initial = 'initial'
+        animate = 'open'
+        className = 'group hover:opacity-50 flex w-full justify-between items-center py-[4vh] px-[50px] border-t border-[#C9C9C9] transition-all duration-200 cursor-pointer last-of-type:border-b'
         onMouseEnter={() => {setModal({active: true, index})}} onMouseLeave={() => {setModal({active: false, index})}}>
         <div className = 'flex items-end'>
+          
           <h2 className = 'group-hover:translate-x-[-10px] m-0 mr-2 transition-all duration-[0.4] font-normal text-[1.8vw]'>{title}</h2>
           <p className = 'opacity-20 font-light'>{year}</p>
         </div>
