e4ba74b4c40469732a1d45320081bbcf6664e822
Thomas Booth <thomasbooth26@gmail.com>
2023-08-12 18:56:41 +0100

    line height thinner

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -38,7 +38,7 @@ const Link: React.FC<linkProps> = ({ title, url, changeRoute }) => {
             className="relative cursor-pointer inline-block overflow-hidden">
             <h2>{title}</h2>
             <motion.div
-            className="absolute left-0 w-full h-[0.2vh] bg-pastel-gray-light bottom-0"
+            className="absolute left-0 w-full h-[2px] bg-pastel-gray-light bottom-0"
             animate={hoverState}
             variants={underlineVariants}
             transition={{ duration: 0.4 }}
