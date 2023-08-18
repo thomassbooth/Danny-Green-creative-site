19bd6f9365a2e6aba9a1521b2aa8071b33976966
Thomas Booth <thomasbooth26@gmail.com>
2023-08-18 20:54:51 +0100

    reimplement locomotive

--- src/components/LocomotiveScrollProvider.tsx
+++ src/components/LocomotiveScrollProvider.tsx
@@ -13,23 +13,23 @@ const LocomotiveScrollProvider = ({
     useEffect( () => {
         (
           async () => {
-          //   const LocomotiveScroll = (await import('locomotive-scroll')).default
-          //   const locomotiveScroll = new LocomotiveScroll({
-          //     lenisOptions: {
-          //         wrapper: window,
-          //         content: document.documentElement,
-          //         lerp: 0.1,
-          //         duration: 1.2,
-          //         orientation: 'vertical',
-          //         gestureOrientation: 'vertical',
-          //         smoothWheel: true,
-          //         smoothTouch: true,
-          //         wheelMultiplier: 1,
-          //         touchMultiplier: 2,
-          //         normalizeWheel: true,
-          //         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-          //     },
-          // })
+            const LocomotiveScroll = (await import('locomotive-scroll')).default
+            const locomotiveScroll = new LocomotiveScroll({
+              lenisOptions: {
+                  wrapper: window,
+                  content: document.documentElement,
+                  lerp: 0.1,
+                  duration: 1.2,
+                  orientation: 'vertical',
+                  gestureOrientation: 'vertical',
+                  smoothWheel: true,
+                  smoothTouch: true,
+                  wheelMultiplier: 1,
+                  touchMultiplier: 2,
+                  normalizeWheel: true,
+                  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+              },
+          })
           }
         )()
       }, [])
