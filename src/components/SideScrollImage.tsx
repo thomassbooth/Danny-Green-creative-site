d1bff5e786cc0250861676df2ff381b212d27b12
Thomas Booth <thomasbooth26@gmail.com>
2023-08-24 19:40:42 +0100

    longer scroll on initial part

--- src/components/SideScrollImage.tsx
+++ src/components/SideScrollImage.tsx
@@ -15,19 +15,19 @@ const SideScrollImage = () => {
       target: containerRef,
     })
 
-    const x = useTransform(scrollYProgress, [0.3, 0.98], ['0vw', '-100vw'])
+    const x = useTransform(scrollYProgress, [0.4, 0.98], ['0vw', '-100vw'])
     
-    const scale = useTransform(scrollYProgress, [0.3, 0.95], [1, 0.1])
+    const scale = useTransform(scrollYProgress, [0.4, 0.95], [1, 0.1])
     const imageScale = useTransform(scrollYProgress, [0, 0.3], [1.2, 1])
 
-    const opacity = useTransform(scrollYProgress, [0.3, 0.9], [1, 0.8])
-    const rounded = useTransform(scrollYProgress, [0, 0.3, 0.35, 0.9], [0, 0, 100, (hasWindow ? window.screen.width/2 : '900')])
+    const opacity = useTransform(scrollYProgress, [0.4, 0.9], [1, 0.8])
+    const rounded = useTransform(scrollYProgress, [0, 0.4, 0.45, 0.9], [0, 0, 100, (hasWindow ? window.screen.width/2 : '900')])
     const slideInX = useTransform(scrollYProgress, [0.6, 0.98], ['20vw', '0vw'])
 
-    const slideImageY = useTransform(scrollYProgress, [0, 0.3], ['0vh', '-30vh'])
-    const slideText1 = useTransform(scrollYProgress, [0, 0.3], ['0vh', '-45vh'])
-    const slideText2X = useTransform(scrollYProgress, [0, 0.3], ['0vh', '-4vw'])
-    const slideText2Y = useTransform(scrollYProgress, [0, 0.3], ['0vh', '-35vh'])
+    const slideImageY = useTransform(scrollYProgress, [0, 0.4], ['0vh', '-30vh'])
+    const slideText1 = useTransform(scrollYProgress, [0, 0.4], ['0vh', '-45vh'])
+    const slideText2X = useTransform(scrollYProgress, [0, 0.4], ['0vh', '-4vw'])
+    const slideText2Y = useTransform(scrollYProgress, [0, 0.4], ['0vh', '-35vh'])
     
   return (
     <>
